016f85622c4ef07d6d6b0b572c1fa645dd05cea9
Luis <rialluis2@gmail.com>
2023-04-25 19:12:01 +0200

    local funcionandooooooooo asincronoooooo

--- src/main/java/prueba/Proceso.java
+++ src/main/java/prueba/Proceso.java
@@ -1,13 +1,16 @@
 package prueba;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.net.URI;
-import java.util.PriorityQueue;
 import java.util.concurrent.Semaphore;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.InvocationCallback;
 
 public class Proceso extends Thread {
 	private int id; // Identificador del proceso
@@ -15,36 +18,57 @@ public class Proceso extends Thread {
 	private int Ti; // Tiempo de solicitud de entrada en sección crítica(Lamport)
 	private Estado estado; // Estado del proceso (LIBERADA, BUSCADA, TOMADA)
 
-	private PriorityQueue<Mensaje> cola; // Cola de peticiones de entrada en sección crítica
+	private Queue<Mensaje> cola; // Cola de peticiones de entrada en sección crítica
 	private int numRespuestas; // Número de respuestas recibidas por el proceso
 	private int nProcesos; // Número total de procesos en el sistema
 	private String ip[] = new String[3];
 
-	private Comparador comparador;
 	private Logger logger;
-
+	private int servidor;
 	private Semaphore semSC;
 
-	public Proceso(int id, int numProcesos, String ip1, String ip2, String ip3) {
+	public Proceso(int id, int numProcesos, String[] ips, int serverId) {
 		this.id = id;
 		this.Ci = 0;
 		this.Ti = 0;
 		this.estado = Estado.LIBERADA;
-		this.cola = new PriorityQueue<>();
+		this.cola = new LinkedList<>();
 		this.numRespuestas = 0;
 		this.nProcesos = numProcesos;
 		this.semSC = new Semaphore(0);
-		this.ip[0] = ip1;
-		this.ip[1] = ip2;
-		this.ip[2] = ip3;
+		this.ip[0] = ips[0];
+		// this.ip[1] = ips[1];
+		// this.ip[2] = ips[2];
+		this.servidor = serverId;
 		this.logger = new Logger("C:\\Users\\Luis\\Desktop\\proceso_" + id + ".log");
-		this.comparador = new Comparador();
 	}
 
 	private void entradaSC() {
 		estado = Estado.BUSCADA;
 		Ti = Ci;
-		peticionMulticast();
+
+		for (int i = 0; i < nProcesos; i++) {
+			if (i != id) {
+
+				Client cliente = ClientBuilder.newClient();
+				URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
+				WebTarget target = cliente.target(uri);
+				target.path("rest").path("servicio").path("multicast").queryParam("Pj", id).queryParam("Tj", Ti)
+						.queryParam("destino", i).request(MediaType.TEXT_PLAIN).async()
+						.get(new InvocationCallback<Response>() {
+							@Override
+							public void completed(Response response) {
+							}
+
+							@Override
+							public void failed(Throwable throwable) {
+								System.out.println("Invocation failed.");
+								throwable.printStackTrace();
+							}
+						});
+			}
+		}
+
 		for (numRespuestas = 0; numRespuestas < (nProcesos - 1); numRespuestas++) {
 			try {
 				semSC.acquire();
@@ -54,105 +78,85 @@ public class Proceso extends Thread {
 			}
 		}
 		estado = Estado.TOMADA;
-		
+		System.out.println("Soy " + id + " y acabo de tomar la SC");
 		String texto = "P" + (id + 1) + " E " + System.currentTimeMillis() + "\n";
 		logger.write(texto);
 
 		Ci = Ci + 1;
 	}
 
-	private void peticionMulticast() {
-		int servidor;
-		for (int i = 0; i < nProcesos; i++) {
-			if (i != id) {
-				if (i == 0 || i == 1)
-					servidor = 0;
-				else if (i == 3 || i == 4)
-					servidor = 1;
-				else
-					servidor = 2;
-
-				Client cliente = ClientBuilder.newClient();
-				URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
-				WebTarget target = cliente.target(uri);
-				target.path("rest").path("servicio").path("multicast").queryParam("Pj", id).queryParam("Tj", Ti)
-						.queryParam("destino", i).request(MediaType.TEXT_PLAIN).get(String.class);
-			}
-		}
-	}
-
 	public void recibirMensaje(int Pj, int Tj) {
 		Ci = Math.max(Ci, Tj) + 1;
-		if (estado == Estado.TOMADA || (estado == Estado.BUSCADA && (comparador.compara(Ti, Tj, id, Pj)))) {
+		if (estado == Estado.TOMADA || (estado == Estado.BUSCADA && (Ti < Tj || (Ti == Tj && id < Pj)))) {
 			Mensaje peticion = new Mensaje(Pj, Tj, TipoMensaje.SOLICITUD_ACCESO);
 			cola.offer(peticion);
+			String texto = "Cola" + (id + 1) + ": " + Pj + ", " + Tj + "\n";
+			logger.write(texto);
 		} else {
-			int servidor;
-			if (Pj == 0 || Pj == 1)
-				servidor = 0;
-			else if (Pj == 3 || Pj == 4)
-				servidor = 1;
-			else
-				servidor = 2;
 
 			Client cliente = ClientBuilder.newClient();
 			URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
 			WebTarget target = cliente.target(uri);
 			target.path("rest").path("servicio").path("respuesta").queryParam("destino", Pj)
-					.request(MediaType.TEXT_PLAIN).get(String.class);
+					.request(MediaType.TEXT_PLAIN).async().get(new InvocationCallback<Response>() {
+						@Override
+						public void completed(Response response) {
+						}
+
+						@Override
+						public void failed(Throwable throwable) {
+							System.out.println("Invocation failed.");
+							throwable.printStackTrace();
+						}
+					});
 		}
 	}
 
 	public void recibirRespuesta() {
-		semSC.release();
+		this.semSC.release();
 	}
 
 	private void salidaSC() {
 		estado = Estado.LIBERADA;
-		
+		System.out.println("Soy " + id + " y acabo de salir");
 		String texto = "P" + (id + 1) + " S " + System.currentTimeMillis() + "\n";
 		logger.write(texto);
-		
-		responderPeticiones();
-	}
 
-	private void responderPeticiones() {
 		while (!cola.isEmpty()) {
 			Mensaje mensaje = cola.poll();
-
-			int servidor;
-			if (mensaje.getIdProceso() == 0 || mensaje.getIdProceso() == 1)
-				servidor = 0;
-			else if (mensaje.getIdProceso() == 3 || mensaje.getIdProceso() == 4)
-				servidor = 1;
-			else
-				servidor = 2;
 
 			Client cliente = ClientBuilder.newClient();
 			URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
 			WebTarget target = cliente.target(uri);
-			target.path("rest").path("servicio").path("respuesta").queryParam("destino", mensaje.getIdProceso())
-					.request(MediaType.TEXT_PLAIN).get(String.class);
-		}
 
+			target.path("rest").path("servicio").path("respuesta").queryParam("destino", mensaje.getIdProceso())
+					.request(MediaType.TEXT_PLAIN).async().get(new InvocationCallback<Response>() {
+						@Override
+						public void completed(Response response) {
+						}
+
+						@Override
+						public void failed(Throwable throwable) {
+							System.out.println("Invocation failed.");
+							throwable.printStackTrace();
+						}
+					});
+
+		}
 	}
 
 	public void run() {
-		float tiempo1;
-		float tiempo2;
-
 		for (int i = 0; i < 100; i++) {
-			tiempo1 = (float) (Math.random() * 500.0 + 300.0);
 			try {
-				Thread.sleep((long) tiempo1);
+				Thread.sleep((long) (Math.random() * 200.0 + 300.0));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 
 			entradaSC();
-			tiempo2 = (float) (Math.random() * 300.0 + 100.0);
+
 			try {
-				Thread.sleep((long) tiempo2);
+				Thread.sleep((long) (Math.random() * 200.0 + 100.0));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
--- src/main/java/prueba/Servicio.java
+++ src/main/java/prueba/Servicio.java
@@ -10,8 +10,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 @Singleton
@@ -43,7 +45,17 @@ public class Servicio {
 			URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
 			WebTarget target = cliente.target(uri);
 			target.path("rest").path("servicio").path("procesos").queryParam("idIp", idIp).queryParam("ips", ips)
-					.request(MediaType.TEXT_PLAIN).get(String.class);
+					.request(MediaType.TEXT_PLAIN).async().get(new InvocationCallback<Response>() {
+						@Override
+						public void completed(Response response) {
+						}
+
+						@Override
+						public void failed(Throwable throwable) {
+							System.out.println("Invocation failed.");
+							throwable.printStackTrace();
+						}
+					});
 		}
 
 		return "Comenzamos";
@@ -53,26 +65,16 @@ public class Servicio {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("procesos")
 	public String procesos(@QueryParam(value = "idIp") int idIp, @QueryParam(value = "ips") String ips) {
-		servidor=idIp;
-		String ipsSplit[]= ips.split(",");
-		
-		if (servidor == 0) {
-			procesos[0] = new Proceso(0, numProcesos, ipsSplit[0], "1", "2" /*ipsSplit[1], ipsSplit[2]*/);
-			procesos[1] = new Proceso(1, numProcesos, ipsSplit[0], "1", "2" /*ipsSplit[1], ipsSplit[2]*/);
-			procesos[0].start();
-			procesos[1].start();
-		} else if (servidor == 1) {
-			procesos[0] = new Proceso(2, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[1] = new Proceso(3, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[0].start();
-			procesos[1].start();
-		} else {
-			procesos[0] = new Proceso(4, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[1] = new Proceso(5, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[0].start();
-			procesos[1].start();
+		servidor = idIp;
+		String[] ipsSplit = ips.split(",");
+
+		int i = 0;
+		while (i < 2) {
+			procesos[i] = new Proceso((2 * servidor) + i, numProcesos, ipsSplit, servidor);
+			procesos[i].start();
+			i++;
 		}
-		
+
 		return "Procesos creados";
 	}
 
@@ -81,21 +83,10 @@ public class Servicio {
 	@Path("multicast")
 	public String multicast(@QueryParam(value = "Pj") int Pj, @QueryParam(value = "Tj") int Tj,
 			@QueryParam(value = "destino") int idProcesoReceptor) {
-		if (servidor == 0) {
-			if (idProcesoReceptor == 0)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 1)
-				procesos[1].recibirMensaje(Pj, Tj);
-		} else if (servidor == 1) {
-			if (idProcesoReceptor == 2)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 3)
-				procesos[1].recibirMensaje(Pj, Tj);
-		} else {
-			if (idProcesoReceptor == 4)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 5)
-				procesos[1].recibirMensaje(Pj, Tj);
+		if ((idProcesoReceptor % 2) == 0) {
+			procesos[0].recibirMensaje(Pj, Tj);
+		} else if ((idProcesoReceptor % 2) != 0) {
+			procesos[1].recibirMensaje(Pj, Tj);
 		}
 
 		return "Realizada la multidifusion";
@@ -105,21 +96,10 @@ public class Servicio {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("respuesta")
 	public String respuesta(@QueryParam(value = "Pj") int Pj, @QueryParam(value = "destino") int idProcesoReceptor) {
-		if (servidor == 0) {
-			if (idProcesoReceptor == 0)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 1)
-				procesos[1].recibirRespuesta();
-		} else if (servidor == 1) {
-			if (idProcesoReceptor == 2)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 3)aaa
-				procesos[1].recibirRespuesta();
-		} else {
-			if (idProcesoReceptor == 4)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 5)
-				procesos[1].recibirRespuesta();
+		if ((idProcesoReceptor % 2) == 0) {
+			procesos[0].recibirRespuesta();
+		} else if ((idProcesoReceptor % 2) != 0) {
+			procesos[1].recibirRespuesta();
 		}
 
 		return "Respuesta enviada";
