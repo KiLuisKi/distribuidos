f2bb13529bae46ea6831dd9ce248746ff571822b
Luis <rialluis2@gmail.com>
2023-05-02 18:27:20 +0200

    local sincrono con nuevos cambios

--- src/main/java/prueba/Servicio.java
+++ src/main/java/prueba/Servicio.java
@@ -2,6 +2,7 @@ package prueba;
 
 import java.net.URI;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -17,11 +18,13 @@ import javax.ws.rs.core.UriBuilder;
 @Singleton
 @Path("servicio")
 public class Servicio {
+	private int espera = 0;
 	private int servidor;
-	private int numProcesos = 2;
-	private int numServidores = 1;
+	private int numProcesos = 4;
+	private int numServidores = 2;
 	private Proceso procesos[] = new Proceso[2];
-
+	private Semaphore semWait = new Semaphore(0);
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("start")
@@ -37,10 +40,10 @@ public class Servicio {
 
 		for (int i = 0; i < numServidores; i++) {
 			int idIp = i;
-			String ips = ip[0];// + "," + ip[1] + "," + ip[2];
+			String ips = ip[0] + "," + ip[1];// + "," + ip[2];
 
 			Client cliente = ClientBuilder.newClient();
-			URI uri = UriBuilder.fromUri("http://" + ip[servidor] + ":8080/prueba").build();
+			URI uri = UriBuilder.fromUri("http://" + ip[idIp] + ":8080/prueba").build();
 			WebTarget target = cliente.target(uri);
 			target.path("rest").path("servicio").path("procesos").queryParam("idIp", idIp).queryParam("ips", ips)
 					.request(MediaType.TEXT_PLAIN).get(String.class);
@@ -53,26 +56,17 @@ public class Servicio {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("procesos")
 	public String procesos(@QueryParam(value = "idIp") int idIp, @QueryParam(value = "ips") String ips) {
-		servidor=idIp;
-		String ipsSplit[]= ips.split(",");
-		
-		if (servidor == 0) {
-			procesos[0] = new Proceso(0, numProcesos, ipsSplit[0], "1", "2" /*ipsSplit[1], ipsSplit[2]*/);
-			procesos[1] = new Proceso(1, numProcesos, ipsSplit[0], "1", "2" /*ipsSplit[1], ipsSplit[2]*/);
-			procesos[0].start();
-			procesos[1].start();
-		} else if (servidor == 1) {
-			procesos[0] = new Proceso(2, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[1] = new Proceso(3, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[0].start();
-			procesos[1].start();
-		} else {
-			procesos[0] = new Proceso(4, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[1] = new Proceso(5, numProcesos, ipsSplit[0], ipsSplit[1], ipsSplit[2]);
-			procesos[0].start();
-			procesos[1].start();
+		servidor = idIp;
+		String[] ipsSplit = ips.split(",");
+
+		int i = 0;
+		while (i < 2) {
+			procesos[i] = new Proceso((2 * servidor) + i, numProcesos, ipsSplit, servidor);
+			procesos[i].start();
+			i++;
 		}
 		
+		semWait.release();
 		return "Procesos creados";
 	}
 
@@ -81,21 +75,10 @@ public class Servicio {
 	@Path("multicast")
 	public String multicast(@QueryParam(value = "Pj") int Pj, @QueryParam(value = "Tj") int Tj,
 			@QueryParam(value = "destino") int idProcesoReceptor) {
-		if (servidor == 0) {
-			if (idProcesoReceptor == 0)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 1)
-				procesos[1].recibirMensaje(Pj, Tj);
-		} else if (servidor == 1) {
-			if (idProcesoReceptor == 2)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 3)
-				procesos[1].recibirMensaje(Pj, Tj);
-		} else {
-			if (idProcesoReceptor == 4)
-				procesos[0].recibirMensaje(Pj, Tj);
-			else if (idProcesoReceptor == 5)
-				procesos[1].recibirMensaje(Pj, Tj);
+		if ((idProcesoReceptor % 2) == 0) {
+			procesos[0].recibirMensaje(Pj, Tj);
+		} else if ((idProcesoReceptor % 2) != 0) {
+			procesos[1].recibirMensaje(Pj, Tj);
 		}
 
 		return "Realizada la multidifusion";
@@ -105,23 +88,38 @@ public class Servicio {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("respuesta")
 	public String respuesta(@QueryParam(value = "Pj") int Pj, @QueryParam(value = "destino") int idProcesoReceptor) {
-		if (servidor == 0) {
-			if (idProcesoReceptor == 0)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 1)
-				procesos[1].recibirRespuesta();
-		} else if (servidor == 1) {
-			if (idProcesoReceptor == 2)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 3)aaa
-				procesos[1].recibirRespuesta();
-		} else {
-			if (idProcesoReceptor == 4)
-				procesos[0].recibirRespuesta();
-			else if (idProcesoReceptor == 5)
-				procesos[1].recibirRespuesta();
+		if ((idProcesoReceptor % 2) == 0) {
+			procesos[0].recibirRespuesta();
+		} else if ((idProcesoReceptor % 2) != 0) {
+			procesos[1].recibirRespuesta();
 		}
 
 		return "Respuesta enviada";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("espera")
+	public String espera() {
+		if (espera == (numProcesos - 1)) {
+			try {
+				semWait.acquire();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int i = 0;
+			while (i < 2) {
+				procesos[i].startProc();
+				i++;
+			}
+		} else {
+			synchronized (this) {
+				espera++;
+			}
+
+		}
+
+		return "Espera realizada";
 	}
 }
